b4d0bec1f69ef100d637c316ed428cb6423e9cd9
Gabriel-Hub-86 <engmecgabriel@gmail.com>
2025-03-25 20:17:59 -0300

    add avarage age students

--- menu.py
+++ menu.py
@@ -2,25 +2,28 @@ from showDataset import ShowDataset
 from anxietyGenderAnalysis import AnxietyGenderAnalysis
 from depressionGenderAnalysis import DepressionGenderAnalysis
 from optionFour import OptionFour
+from avarageAgeStudents import AvarageAgeStudents
+
 
 class Menu:
-    def __init__(menu): ## Contrutor da classe Menu
+    def __init__(menu):  # Contrutor da classe Menu
         menu.options = {
             "1": ShowDataset(),
             "2": AnxietyGenderAnalysis(),
             "3": DepressionGenderAnalysis(),
             "4": OptionFour(),
+            "6": AvarageAgeStudents(),
             "0": None
         }
 
     def displayTitle():
         print("Aqui vai o Nome do Projeto")
-    
-    def displayMenu(menu): ## Método para visualização do menu
+
+    def displayMenu(menu):  # Método para visualização do menu
         print("\nMenu de Opções:")
         print("1 - Mostar Dataset")
         print("2 - Mostrar Ansiedade por Gênero")
         print("3 - Mostrar Depressão por Gênero")
         print("4 - Funcionalidade 4")
+        print("6 - Média de idade dos estudantes")
         print("0 - Sair")
-
