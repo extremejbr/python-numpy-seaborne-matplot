4bfe4be4c034cfa12010fb3f9bd47a242507ca46
Gabriel-Hub-86 <engmecgabriel@gmail.com>
2025-03-25 22:32:09 -0300

    Add Pie Chart Anxiety By Age

--- menu.py
+++ menu.py
@@ -6,9 +6,11 @@ from findByColumn import FindByColumn
 from findValue import FindValue
 from depressionAnxietyPercentualAnalysis import DepressionAnxietyPercentualAnalysis
 from studentesInterviewsByDay import StudentesInterviewsByDay
+from pieChartAnxietyByAge import PieChartAnxietyByAge
+
 
 class Menu:
-    def __init__(menu): ## Construtor da classe Menu
+    def __init__(menu):  # Construtor da classe Menu
         menu.options = {
             "1": ShowDataset(),
             "2": AnxietyGenderAnalysis(),
@@ -18,13 +20,14 @@ class Menu:
             "6": AvarageAgeStudents(),
             "7": DepressionAnxietyPercentualAnalysis(),
             "8": StudentesInterviewsByDay(),
+            "9": PieChartAnxietyByAge(),
             "0": None
         }
 
     def displayTitle():
         print("Análises em Dataset de Transtornos de Humor")
-        
-    def displayMenu(menu): ## Método para visualização do menu
+
+    def displayMenu(menu):  # Método para visualização do menu
         print("\nMenu de Opções:")
         print("1 - Mostar Dataset")
         print("2 - Mostrar Ansiedade por Gênero")
@@ -34,4 +37,5 @@ class Menu:
         print("6 - Média de idade dos estudantes")
         print("7 - Percentual de estudantes com Transtorno Mental")
         print("8 - Total de entrevistas por dia")
+        print("9 - Percentual de estudantes com Ansiedade por Idade")
         print("0 - Sair")
